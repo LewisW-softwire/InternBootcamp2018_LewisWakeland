4c7fab57421e0764be6c031869df63ea60c21a8e
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-06-26 15:09:32 +0100

    Added additional rules to FizzBuzz

--- FizzBuzz/index.js
+++ FizzBuzz/index.js
@@ -1,12 +1,35 @@
 
-for(var i = 1; i<=100; i++){
-    if(i%3 === 0 && i%5 == 0){
-        console.log('FizzBuzz');
-    }else if(i%3 === 0){
-        console.log('Fizz');
-    }else if(i%5 === 0){
-        console.log('Buzz');
-    }else{
-        console.log(i);
+for(var i = 1; i<=1000; i++){
+    let output = [];
+    if(i%3 === 0){ //Simple appending of words
+        output.push('Fizz');
     }
+    if(i%5 === 0){
+        output.push('Buzz');
+    }
+    if(i%7 === 0){
+        output.push('Bang');
+    }
+    if(i%11 === 0){ //Bong replaces any previous items
+        output = ['Bong'];
+    }
+    if(i%13 === 0){ //Fezz goes immediately in front of the first item beginning with a B, and is printed even when Bong is present
+        for(let j = 0; j<output.length; j++){
+            if(output[j].charAt(0).toUpperCase() === 'B'){
+                output.splice(j, 0, 'Fezz');
+                break;
+            }
+            if(j === output.length-1){
+                output.push('Fezz');
+                break;
+            }
+        }
+    }
+    if(i%17 === 0){ //Reverse the order of the words
+        output.reverse();
+    }
+    if(output.length === 0){
+        output.push(i).toString();
+    }
+    console.log(`${i}: ${output.join("")}`);
 }
