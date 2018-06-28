1239a44355cdd67f6352063395b10353ffecd313
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-06-28 14:53:16 +0100

    Added data validation

--- Supportbank/index.mjs
+++ Supportbank/index.mjs
@@ -2,20 +2,41 @@
 
 import accountsParser from "./accountsParser.mjs";
 import accountActivities from "./accountActivities.mjs";
+import validateData from "./validateData.mjs";
 import readline from 'readline-sync';
-import moment from 'moment';
 import fs from 'fs';
 import path from 'path';
+import log4js from 'log4js';
+import moment from 'moment';
 
-//Main program
+log4js.configure({
+    appenders: {
+        file: { type: 'fileSync', filename: 'logs/debug.log' }
+    },
+    categories: {
+        default: { appenders: ['file'], level: 'debug'}
+    }
+});
+const logger = log4js.getLogger('index');
 
+//-----Main program-----
+logger.info(`Dear diary, the user started running me at ${moment.now()}. They seem nice, I think we'll get along.`);
 let __dirname = path.resolve(path.dirname(''));
-let fileLocation = path.join(__dirname, '/Transactions2014.csv')
+let fileName = '/Transactions2014.csv';
+let fileLocation = path.join(__dirname, fileName);
 //Only continue if the file is found
 if(fs.existsSync(fileLocation)){
+    logger.info(`Dear diary, ooh, she's feeding me a file. It's called ${fileName} - that sounds tasty, I can't wait!`);
     let file = fs.readFileSync(fileLocation);
     let [transactions, accounts] = accountsParser.parseCSV(file);
-    promptCommand(accounts);
+    if(validateData.validateTransactions(transactions)){
+        promptCommand(accounts);
+    }else{
+        logger.error("Dear diary, I die. :-(");
+        console.log("Transaction data not valid, see log file for details.");
+    }
+}else{
+    logger.error(`File "${fileLocation}" not found, exiting.`);
 }
 
 function promptCommand(accounts){
---
+++ Supportbank/validateData.mjs
@@ -0,0 +1,24 @@
+import moment from 'moment';
+import log4js from 'log4js';
+import path from 'path';
+
+const logger = log4js.getLogger('index');
+
+export default class{
+    static validateTransactions(transactions){
+        let valid = true;
+        for(var index in transactions){
+            let transaction = transactions[index];
+            if(!moment(transaction.date, "DD/MM/YYYY").isValid()){
+                //logger.error(`Invalid date for transaction on line ${parseInt(index)+2} with date: "${transaction.date}"`); //+2 for titles and 0 indexing
+                logger.error(`Dear diary, line ${parseInt(index)+2}! That's not a date! Oh how it burns! Why, ${process.env['USERPROFILE'].split(path.sep)[2]}, why?!?`);
+                valid = false;
+            }
+            if(isNaN(transaction.amount)){
+                logger.error(`Dear diary, line ${parseInt(index)+2}! That's not a number! Oh how it burns! Why, ${process.env['USERPROFILE'].split(path.sep)[2]}, why?!?`);
+                valid = false;
+            }
+        }
+        return valid;
+    }
+}
