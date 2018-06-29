49a67cdfbd7980f344f578dcf2e689701372dc85
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-06-29 11:16:46 +0100

    fetch data from API

--- BusBoard/index.mjs
+++ BusBoard/index.mjs
@@ -1,11 +1,36 @@
 var readline = require('readline-sync');
 var request = require('request');
+var moment = require('moment');
+var Table = require('cli-table');
 
 let stopCode = readline.question("Input stop code: ");
 
-request
-    .get(`https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals?app_id=ad55981b&app_key=cfbb21febb5756cf78e722fefc2d929b`)
-    .on('response', function(response) {
-        console.log(response);
+request(`https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals?app_id=ad55981b&app_key=cfbb21febb5756cf78e722fefc2d929b`,
+    function(error, response, body) {
+        let data = JSON.parse(body);
+        let next5Busses = [];
+        for(let i = 0; i < data.length; i++){
+            let arrivalTime = moment(data[i].expectedArrival).unix();
+            next5Busses.push([i, arrivalTime]);
+            console.log(arrivalTime);
+            /*for( (key, value) in next5Busses){
+                console.log(value);
+            }*/  
+        }
+        next5Busses.sort( function(first, second) {
+            return first[1] - second[1]
+        });
+        next5Busses = next5Busses.slice(0, 5);
+
+        for(let i = 0; i < 5; i++){
+            let currentIndex = next5Busses[i][0]; //the index of the bus we want
+            let currentDestination = data[currentIndex].destinationName;
+            let lineName = data[currentIndex].lineName;
+            let arrivalTime = data[currentIndex].expectedArrival;
+            console.log(currentDestination);
+            console.log(lineName);
+            console.log(arrivalTime);
+
+        }
         
     });
